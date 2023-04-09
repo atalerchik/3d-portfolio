7c67b75f85f69069277e5b028b568a88d4f6eaf8
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-04-09 21:27:12 +0300

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components";
+import { Home } from "./pages/home/Home";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="text-stone-100">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
