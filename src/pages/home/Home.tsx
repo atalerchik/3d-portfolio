2ccf6f08459c0d5fe236d28549622da629df02e5
Talerchik-Aleksey <atalerchik@twelvedevs.com>
2023-04-10 22:19:49 +0300

    feat: add stars for home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,38 @@
+import Star from "../../assets/star.svg";
+
+const usersReviewsPhotos = ["person-4", "person-2", "person-3", "person-4", "person-2", "person-3"];
+
 export function Home() {
   return (
     <div className="bg-[url('/home-background.jpg')] bg-cover h-screen">
       <div className=" flex items-center h-screen w-5/6 mx-auto">
         <div className="">
           <h1 className="text-6xl md:text-8xl font-bold block"> Hi, I'm Alexey. </h1>
-          <p className="text-1xl md:text-2xl my-10 text-slate-300">
+          <p className="text-1xl md:text-2xl my-5 text-slate-300">
             I am a 3D interior designer, at the moment I have more than 100 works
           </p>
+          <div className="flex mb-10 gap-5">
+            <div className="flex gap-1 content-center items-center">
+              <div className="flex">
+                <img src={Star} alt="User raiting" className="w-6 h-5" />
+                <img src={Star} alt="User raiting" className="w-6 h-5" />
+                <img src={Star} alt="User raiting" className="w-6 h-5" />
+                <img src={Star} alt="User raiting" className="w-6 h-5" />
+                <img src={Star} alt="User raiting" className="w-6 h-5" />
+              </div>
+            </div>
+            <div className="flex ml-2">
+              {usersReviewsPhotos.map((photoName) => {
+                return (
+                  <img
+                    src={photoName + ".svg"}
+                    alt={photoName}
+                    className="w-8 h-8 bg-cover rounded-full -ml-2"
+                  />
+                );
+              })}
+            </div>
+          </div>
           <div className="flex gap-8 w-100 text-slate-300">
             <button className="rounded-full border-stone-300 border-2 py-4 flex-1 hover:bg-blue-100 hover:text-slate-900">
               VIEW MY WORKS
